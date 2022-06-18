1774972e533e1fc87f619f86dffd9a9afb9c47a8
Necron090 <jcoc1@aluno.ifal.edu.br>
2022-06-18 16:53:21 -0300

    Prototipo dos botões e barra de navegação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bookstop/pages/login.dart';
 import 'package:bookstop/pages/materia.dart';
 
 
--- lib/pages/materia.dart
+++ lib/pages/materia.dart
@@ -13,7 +13,109 @@ class _MateriaState extends State<Materia> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: const Text("Materia"),
+        child: ListView(
+          children:[ 
+            Text("Livros Recomendados"),
+            SizedBox(height: 40),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.greenAccent
+              ),
+              child: ListView(
+                children: [
+                  ElevatedButton(
+              onPressed: null, 
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+              ),
+              child: const Text(
+                'PROGRAMAÇÂO MOBILE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: null, 
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+              ),
+              child: const Text(
+                'PROGRAMAÇÂO MOBILE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: null, 
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+              ),
+              child: const Text(
+                'PROGRAMAÇÂO MOBILE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: null, 
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+              ),
+              child: const Text(
+                'PROGRAMAÇÂO MOBILE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: null, 
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+              ),
+              child: const Text(
+                'PROGRAMAÇÂO MOBILE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+                ],
+              ),
+            ),
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Color.fromARGB(255, 240, 151, 92),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white.withOpacity(.60),
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+
+            items: [
+              BottomNavigationBarItem(
+                label: 'Biblioteca',
+                icon: Icon(Icons.book_online),
+              ),
+              BottomNavigationBarItem(
+                label: 'Matérias',
+                icon: Icon(Icons.bar_chart_sharp),
+              ),
+            ],
+          )
+          ],
+          )
       ),
     );
   }
