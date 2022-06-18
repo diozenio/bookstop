7298cef551c90e0f73e6de46aafe4cea607961be
diozenio <d.dionisiobsl@gmail.com>
2022-06-18 12:54:02 -0300

    Imagem de fundo do cadastro

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -11,29 +11,41 @@ class _CadastroState extends State<Cadastro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF2F2F2),
-      body: Column(
-        children: [
-          Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.green,
-                child: Center(
-                    child: const Text(
-                  "Crie sua conta",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                      fontFamily: "Times New Roman",
-                      color: Color(0xFFF2F2F2)),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                "https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1366919.jpg&fm=jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Container(
+                  child: Center(
+                      child: const Text(
+                    "Crie sua conta",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        fontFamily: "Times New Roman",
+                        color: Color(0xFFF2F2F2)),
+                  )),
                 )),
-              )),
-          Expanded(
+            Expanded(
               flex: 6,
-              child: Column(
-                children: [],
+              child: Container(
+                  decoration: BoxDecoration(
+                color: Color(0xFFF2F2F2),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16)),
               )),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
