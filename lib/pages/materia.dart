16a977d30ce595a45cb7cdf51ba0d6230dcc59d0
Necron090 <jcoc1@aluno.ifal.edu.br>
2022-06-18 22:27:40 -0300

    Botões (aparentemente) sem cor implementados

--- lib/pages/materia.dart
+++ lib/pages/materia.dart
@@ -20,18 +20,134 @@ class _MateriaState extends State<Materia> {
           padding: EdgeInsets.all(20.0),
           margin: EdgeInsets.all(20.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)
-                  ))
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
                 ),
-                onPressed: null, 
-                child: Text('Materia 1'),
+                child: Text( 'HTML',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
                 ),
-              
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'JavaScript',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'CSS',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'Infraestrutura de Redes',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'Java',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'Python',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250.0, 60.0),
+                  primary: Colors.amberAccent,
+                  onPrimary: Colors.amberAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  )
+                ),
+                child: Text( 'Manutenção de Computadores',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
             ],
           )
         )
