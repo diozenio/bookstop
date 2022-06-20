8d0ebed10436c4b819ab43ea5078b28dc63a448f
renata <90646434+renatabfs@users.noreply.github.com>
2022-06-20 08:00:58 -0300

    Bottom overflowed resolvido.
    :D

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, duplicate_ignore, prefer_const_literals_to_create_immutables, use_full_hex_values_for_flutter_colors
-
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -26,154 +24,167 @@ class _LoginState extends State<Login> {
         child: Column(
           children: [
             Expanded(
-                child: Container(
-                  child: Center(
-                      child: Column (  
-                      mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                        Center(
-                        child: CircleAvatar (
-                        backgroundImage: NetworkImage('https://imgur.com/Q1MHtHH'),
-                        radius: 60,
+              child: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          backgroundImage:
+                              NetworkImage('https://imgur.com/Q1MHtHH'),
+                          radius: 60,
                         ),
-                        ),
-                        
-                        Text('Login',
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                        fontFamily: "Roboto",
-                        color: Color(0xFF070707))
-                          )
-                        ],
                       ),
-                      ),
+                      Text('Login',
+                          // ignore: prefer_const_constructors
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              fontFamily: "Roboto",
+                              color: Color(0xFF070707)))
+                    ],
+                  ),
                 ),
-                ),
-                
+              ),
+            ),
             Expanded(
               child: Container(
                 width: double.infinity,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+                  padding: const EdgeInsets.fromLTRB(30, 50, 30, 10),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container (
-                     child: Column(children: [
-                   Container(decoration: BoxDecoration(
-                  color: Color(0x0FFE8D9CC),
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                ),
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Text('Faça login com seu e-mail',
-                        style: TextStyle(
-                          color: Color(0x0FF2C1810),
+                      Container(
+                        child: Column(children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Color(0x0FFE8D9CC),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(16)),
+                            ),
+                            width: double.infinity,
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Faça login com seu e-mail',
+                                  style: TextStyle(
+                                    color: Color(0x0FF2C1810),
                                     fontWeight: FontWeight.bold,
                                     fontFamily: "Roboto",
                                     fontSize: 16,
-                        ),
-                        ),
-                        Padding(
-                        padding: const EdgeInsets.fromLTRB(45, 50, 60, 90),
-                        child: Column(
-                          children:[ Form(
-                            autovalidateMode: AutovalidateMode.always,
-                            child: TextFormField(
-                              autofocus: true,
-                              style: TextStyle(
-                                  color: Color(0xFF292525),
-                                  fontFamily: "Roboto",
-                                  fontWeight: FontWeight.w100),
-                              decoration: InputDecoration(
-                                  hintText: "exemple@email.com",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                    borderSide: BorderSide(
-                                      width: 0,
-                                      style: BorderStyle.none,
-                                    ),
                                   ),
-                                  fillColor: Color(0xFFDCDDBC),
-                                  filled: true,
-                                  contentPadding: EdgeInsets.all(25)),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Form(
-                            autovalidateMode: AutovalidateMode.always,
-                            child: TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              autofocus: true,
-                              style: TextStyle(
-                                  color: Color(0xFF292525),
-                                  fontFamily: "Roboto",
-                                  fontWeight: FontWeight.w100),
-                              decoration: InputDecoration(
-                                  hintText: "**********",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                    borderSide: BorderSide(
-                                      width: 0,
-                                      style: BorderStyle.none,
-                                    ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(45, 50, 60, 90),
+                                  child: Column(
+                                    children: [
+                                      Form(
+                                        autovalidateMode:
+                                            AutovalidateMode.always,
+                                        child: TextFormField(
+                                          autofocus: true,
+                                          style: TextStyle(
+                                              color: Color(0xFF292525),
+                                              fontFamily: "Roboto",
+                                              fontWeight: FontWeight.w100),
+                                          decoration: InputDecoration(
+                                              hintText: "exemple@email.com",
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                borderSide: BorderSide(
+                                                  width: 0,
+                                                  style: BorderStyle.none,
+                                                ),
+                                              ),
+                                              fillColor: Color(0xFFDCDDBC),
+                                              filled: true,
+                                              contentPadding:
+                                                  EdgeInsets.all(25)),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Form(
+                                        autovalidateMode:
+                                            AutovalidateMode.always,
+                                        child: TextFormField(
+                                          keyboardType:
+                                              TextInputType.emailAddress,
+                                          autofocus: true,
+                                          style: TextStyle(
+                                              color: Color(0xFF292525),
+                                              fontFamily: "Roboto",
+                                              fontWeight: FontWeight.w100),
+                                          decoration: InputDecoration(
+                                              hintText: "**********",
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                borderSide: BorderSide(
+                                                  width: 0,
+                                                  style: BorderStyle.none,
+                                                ),
+                                              ),
+                                              fillColor: Color(0xFFDCDDBC),
+                                              filled: true,
+                                              contentPadding:
+                                                  EdgeInsets.all(25)),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            'Esqueci minha senha',
+                                            textAlign: TextAlign.left,
+                                            style: TextStyle(
+                                              decoration:
+                                                  TextDecoration.underline,
+                                              color: Color(0x0FF004462),
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.all(10),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {},
+                                            child: Padding(
+                                              padding: EdgeInsets.all(10),
+                                              child: Text(
+                                                "Login",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily: "Roboto",
+                                                    fontSize: 16),
+                                              ),
+                                            ),
+                                            style: TextButton.styleFrom(
+                                              primary: Color(0x0FFD9B79A),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
                                   ),
-                                  fillColor: Color(0xFFDCDDBC),
-                                  filled: true,
-                                  contentPadding: EdgeInsets.all(25)),
+                                ),
+                              ],
                             ),
                           ),
-                         Row(
-                          children: [Text('Esqueci minha senha',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Color(0x0FF004462),
-                            fontSize: 15,
-                          ),
-                          ),
-                          Padding(
-                          padding: EdgeInsets.all(10),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Padding(
-                            padding: EdgeInsets.all(10),
-                              child: Text(
-                                "Login",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: "Roboto",
-                                    fontSize: 16),
-                              ),
+                          Text(
+                            'Não tem login? cadraste-se',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Color(0x0FF004462),
+                              fontSize: 15,
                             ),
-                            style: TextButton.styleFrom(
-                            primary: Color(0x0FFD9B79A),
-                            ),
-                          ),
-                          ],
-                         ),
-                          ],
-                          ),
-                        ),   
-                        
-                      ],
-                    ),
-                          ),
-                          Text('Esqueci minha senha',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Color(0x0FF004462),
-                            fontSize: 15,
-                          ),
                           ),
                         ]),
                       ),
@@ -190,9 +201,9 @@ class _LoginState extends State<Login> {
                 decoration: BoxDecoration(
                   color: Color(0xFFF2F2F2),
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                ),
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
                 ),
               ),
             ),
